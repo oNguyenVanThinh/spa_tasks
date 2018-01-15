c570dac72d751f9da58e284f081f9798b7b5653c
oNguyenVanThinh <nguyen.van.thinh@framgia.com>
2018-01-15 15:34:22 +0700

    Flexbox main content, board, task

--- templates/main.js
+++ templates/main.js
@@ -3,6 +3,14 @@ $('#notification').click(function(e) {
   toggleSidebar();
 })
 
+$('#navsearch').focus(function(e){
+  toggleSidebar();
+})
+
+$('#navsearch').focusout(function(e){
+  toggleSidebar();
+})
+
 function toggleSidebar() {
   if ($('#sidebar').css('flex-basis') == '0px') {
     $('#sidebar').css('flex-basis', '400px')
