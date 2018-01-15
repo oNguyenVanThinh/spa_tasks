10f87a0edb53fbd62bfdefc9e951e69f5764fec4
oNguyenVanThinh <nguyen.van.thinh@framgia.com>
2018-01-15 21:24:51 +0700

    Add modal

--- templates/main.js
+++ templates/main.js
@@ -18,3 +18,21 @@ function toggleSidebar() {
     $('#sidebar').css('flex-basis', '0')
   }
 }
+
+var modalTaskShow = $('#modal-task-show');
+
+$('.task-name').click(function(e) {
+    e.preventDefault();
+    modalTaskShow.css('display', 'block');
+})
+
+$('.modal-close').click(function() {
+    modalTaskShow.css('display', 'none');
+})
+
+$(window).click(function(e){
+  if (e.target == modalTaskShow){
+    modalTaskShow.css('display', 'none');
+  }
+})
+
